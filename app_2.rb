26960b7b3ac300c31d9bffb92ba207ac481e4f16
Lionel Humbert <helloliohumb@gmail.com>
2021-04-27 15:08:10 +0200

    Le combat

--- app_2.rb
+++ app_2.rb
@@ -19,3 +19,58 @@ puts ""
 user = HumanPlayer.new(user_name)
 
 players_arr = [player1 = Player.new("Josiane"), player2 = Player.new("José")]
+
+while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
+
+  puts ""
+  puts ""
+  puts "OK ! #{user_name}, ton état est :"
+  puts user.show_state
+  puts "Je te montre aussi les états des méchants :"
+  puts player1.show_state
+  puts player2.show_state
+
+  puts ""
+  puts "On commence par quoi ?"
+  puts ""
+  puts "a — tu pars à la recherche d'une arme"
+  puts "s — tu pars à la recherche d'un pack de soin"
+  puts ""
+  puts ""
+
+  puts "Si tu veux faire une attaque !"
+  puts ""
+  if player1.life_points > 0
+    print "0 — "
+    player1.show_state
+  end
+  if player2.life_points > 0
+    print "1 — "
+    player2.show_state
+  end
+  puts ""
+
+  puts ""
+  puts "Fait ton choix :"
+  print "• "
+  action = gets.chomp
+  puts ""
+  puts ""
+
+  if action == "a"
+    user.search_weapon
+  end
+
+  if action == "s"
+    user.search_health_pack
+  end
+
+  if action == "0" && player1.life_points > 0
+    user.attacks(player1)
+  end
+
+  if action == "1" && player2.life_points > 0
+    user.attacks(player2)
+  end
+
+end
