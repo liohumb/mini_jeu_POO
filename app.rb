3d9f8b29e140b6b847384a0145c6a5ab1e1a8e30
Lionel Humbert <helloliohumb@gmail.com>
2021-04-27 12:10:54 +0200

    orchestrons un combat app

--- app.rb
+++ app.rb
@@ -4,4 +4,24 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-binding.pry
+player1 = Player.new("Josiane")
+player2 = Player.new("José")
+
+while player1.life_points > 0 && player2.life_points > 0
+
+  puts "Voici l'état de chaque joueur :"
+  print player1.show_state
+  print player2.show_state
+
+  puts "Passons à la phase d'attaque :"
+  puts player1.attacks(player2)
+
+  if player2.life_points <= 0
+    break
+  else
+    puts "Contre attaque de #{player2.name} !"
+    puts player2.attacks(player1)
+  end
+  
+end
+#binding.pry
