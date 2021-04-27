4d579a157d0093a7f02df27ee698295beafb1adb
Lionel Humbert <helloliohumb@gmail.com>
2021-04-27 18:23:05 +0200

    my game

--- app_3.rb
+++ app_3.rb
@@ -3,3 +3,30 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
+
+puts ""
+puts "———————————————————————————————————————————————————"
+puts "•    Bienvenue sur 'ILS VEULENT TOUS MA POO' !    •"
+puts "• Le but du jeu est d'être le dernier survivant ! •"
+puts "———————————————————————————————————————————————————"
+puts ""
+
+puts "Choisi ton pseudo :"
+print "• "
+user_name = gets.chomp
+
+puts ""
+puts ""
+
+my_game = Game.new(user_name)
+
+while my_game.is_still_ongoing? == true do
+  my_game.show_players
+  my_game.menu
+  puts "Choisi une option"
+  action = gets.chomp.to_s
+  puts ""
+  my_game.menu_choice(action)
+  my_game.enemies_attack
+  my_game.end
+end
