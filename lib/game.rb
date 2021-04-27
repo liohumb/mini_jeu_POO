8bb53ea43a5454e6cb3b78691115d785f0dee101
Lionel Humbert <helloliohumb@gmail.com>
2021-04-27 16:07:35 +0200

    initialize Game

--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,9 @@ class Game
 
   attr_accessor :human_player, :enemies
 
+  def initialize(human_player_name)
+    @human_player = HumanPlayer.new(human_player_name)
+    @enemies = [Player.new("Leonardo"), Player.new("Donatello"), Player.new("Michelangelo"), Player.new("Raphaëlo")]
+  end
+
 end
