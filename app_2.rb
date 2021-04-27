b9c4adad1708be4312a1718e0fefbf2d453c1b55
Lionel Humbert <helloliohumb@gmail.com>
2021-04-27 14:18:49 +0200

    Accueil

--- app_2.rb
+++ app_2.rb
@@ -3,3 +3,10 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
+
+puts ""
+puts "———————————————————————————————————————————————————"
+puts "•    Bienvenue sur 'ILS VEULENT TOUS MA POO' !    •"
+puts "• Le but du jeu est d'être le dernier survivant ! •"
+puts "———————————————————————————————————————————————————"
+puts ""
